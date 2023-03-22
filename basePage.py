97987d2db3d3e95264243d3dd175df13749ee697
wangyunhe1210@gmail.com <wangyunhe1210@gmail.com>
2023-03-22 16:29:29 +0800

    init

--- basePage.py
+++ basePage.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from driver import Driver
+from utils.handle_yaml import read_yaml
 
 
 class BasePage:
@@ -8,3 +9,14 @@ class BasePage:
         self.login_url = 'https://work.weixin.qq.com/wework_admin/loginpage_wx?from=myhome_baidu'
         self.base_url = 'https://work.weixin.qq.com/wework_admin/frame'
 
+    def __login(self):
+        self.driver.get(self.login_url)
+        for cookie in read_yaml(r'./cookies.yaml'):
+            self.driver.add_cookie(cookie)
+        self.driver.get(self.base_url)
+
+        return self.driver
+
+
+if __name__ == '__main__':
+    page = BasePage()
